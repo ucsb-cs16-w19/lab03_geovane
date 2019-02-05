bb09c10d3c8517743119f5e746d7816639f62a20
Geovane Marquez <geovane@cs.ucsb.edu>
2019-02-05 11:32:27 -0800

    Adding Lab3 files

---
+++ Makefile
@@ -0,0 +1,4 @@
+myProg01: myProg01.cpp
+	g++ myProg01.cpp -o myProg01
+countDucks: countDucks.cpp
+	g++ countDucks.cpp -o countDucks
---
+++ myProg01.cpp
@@ -0,0 +1,18 @@
+// myProg01.cpp
+// Author: Geovane Marquez
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    // Simple program that counts down from n to 1
+ 
+    int n=5;
+ 
+    for (int i=n; i>0; i--) {
+       cout << i << " ";
+    }
+    cout << endl;
+ 
+    return 0;
+}
+
