9934327ae5ed0cbcba9635e5f58d993cd2264a89
Geovane Marquez <geovane@cs.ucsb.edu>
2019-02-08 14:39:12 -0800

    New files

--- myProg02.cpp
+++ myProg02.cpp
@@ -1,11 +1,12 @@
-// countDucks.cpp 
-// Author: Geovane Marquez
-// Example program to read from file and count occurences
+// myProg02.cpp
+// Geovane Marquez
+// Preconditions: Program reads from a text file and counts occurences
+// Postcondtions: The number of ducks counted and the numbers of non ducks counted will be printed to screen.
 
 #include <iostream> // for printf()
+#include <string>   // for string 
 #include <cstdlib> // for exit(), perror()
 #include <fstream> // for ifstream
-
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -16,18 +17,24 @@ int main(int argc, char *argv[])
     	cerr << "Usage: "<< argv[0] << " inputFile" << endl;
     	exit(1); // defined in cstdlib
     } 
-  string str;
+  string str ="";
   ifstream in(argv[1]); //add inputfile
   int animalCount = 0;
   int duckCount = 0;
   int nonduckCount = 0;
-  while(getline(in, str)) //loop the lines
+  while(!in.eof()) //loop the lines
   {
+	getline(in, str);
 	if (str == "duck")
+	{
 		duckCount++;
-  	else 
-		nonduckCount++;
-	animalCount++;
+		animalCount++;
+	}
+  	else if (str != "") // if the line is not empty
+	{
+		nonduckCount++;		
+		animalCount++;
+	}		
   }	  
   in.close(); //close file 
   cout << "Report for " << argv[1] <<":" << endl;
